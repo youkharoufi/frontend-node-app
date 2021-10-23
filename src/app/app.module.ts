1dcb65746facee08256b33eed260c217cdfa3cd2
unknown <y.kharoufi96@gmail.com>
2021-10-23 02:15:41 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/auth/signup/signup.component';
+import { SigninComponent } from './components/auth/signin/signin.component';
+import { ShopComponent } from './components/shop/shop.component';
+import { SingleProductComponent } from './components/shop/single-product/single-product.component';
+import { EditProductComponent } from './components/shop/edit-product/edit-product.component';
+import { CartComponent } from './components/shop/cart/cart.component';
+import { HeaderComponent } from './components/partials/header/header.component';
+import { FooterComponent } from './components/partials/footer/footer.component';
+import { NotFoundComponent } from './components/partials/not-found/not-found.component';
+import { HeaderPageComponent } from './components/partials/header-page/header-page.component';
+import { DeleteProductModalComponent } from './components/partials/modal/delete-product-modal/delete-product-modal.component';
+import { AddProductComponent } from './shop/add-product/add-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    ShopComponent,
+    SingleProductComponent,
+    EditProductComponent,
+    CartComponent,
+    HeaderComponent,
+    FooterComponent,
+    NotFoundComponent,
+    HeaderPageComponent,
+    DeleteProductModalComponent,
+    AddProductComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/partials/modal/delete-product-modal/delete-product-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'node-delete-product-modal',
+  templateUrl: './delete-product-modal.component.html',
+  styleUrls: ['./delete-product-modal.component.css']
+})
+export class DeleteProductModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
